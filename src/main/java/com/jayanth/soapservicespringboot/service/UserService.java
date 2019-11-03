7883cdc6868c4eaa18984840b9be1624d92a2b70
jayan <jayan@DESKTOP-A8EJLJ4>
2019-11-04 01:46:42 +0530

    user details

--- src/main/java/com/jayanth/soapservicespringboot/service/UserService.java
+++ src/main/java/com/jayanth/soapservicespringboot/service/UserService.java
@@ -23,18 +23,18 @@ public class UserService {
 		user.put(user1.getName(), user1);
 		
 		User user2 = new User();
-		user2.setEmpId(11075356);
+		user2.setEmpId(11064356);
 		user2.setName("user2");
 		user2.setSalary(37000.00);
 		
-		user.put(user2.getName(), user1);
+		user.put(user2.getName(), user2);
 		
 		User user3 = new User();
-		user3.setEmpId(11075356);
+		user3.setEmpId(11043356);
 		user3.setName("user3");
 		user3.setSalary(38000.00);
 		
-		user.put(user3.getName(), user1);
+		user.put(user3.getName(), user3);
 	}
 	
 	public User getUsers(String name) {
